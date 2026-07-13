0ce34d6ea24ef2aaf0fa73fe1d7b259406baeab7
divineshedrack33220 <divineshedrack1@gmail.com>
2026-07-13 14:29:19 +0100

    fine tuning

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "/api";
 
 export const SITE_NAME = "TKAYKONCEPTS INT'L";
 export const SITE_DESCRIPTION = "Creating products that inspire people to live boldly and purposefully.";
